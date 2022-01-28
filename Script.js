be18078b14dbdf5b712c43a33726a3d81ed53a5d
Habibsultani <a.habibsultani@gmail.com>
2022-01-28 21:31:26 +0430

    git init

--- Script.js
+++ Script.js
@@ -2,10 +2,46 @@ console.log(12 + 65 + 56 + 645);
 let js = "Amazing";
 console.log(js);
 
-//This is my first time I code in javaScript..
+//This is my first time, that I am coding in javaScript..
 
 let firstName = "Habib";
 let lastName = "Sultani";
 
 console.log(firstName);
 console.log(lastName);
+
+// corse assignment
+
+/*
+let contonent = "Asia";
+let country = "Afghanistan";
+let population = "32 million";
+console.log(contonent);
+console.log(country);
+console.log(population);
+
+country = "Afghanistan";
+console.log(country);
+*/
+
+// let country = "Afghanistan";
+// country = "Afghanistan";
+// console.log(country);
+
+const contonent = "Asia";
+const country = "Afghanistan";
+
+console.log(contonent > country);
+
+const mybiryear = "2022-20";
+const currentage = "2022-2002";
+console.log(2022 - 20, 2022 - 2002);
+
+const mainlanguage = "Dari";
+let language = "pasto";
+console.log(mainlanguage);
+console.log(language);
+language = "Uzbaki";
+console.log(language);
+language = "Hazaragi";
+console.log(language);

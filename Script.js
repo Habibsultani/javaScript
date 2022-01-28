2df33914b3a4a83d8da633feb87897cd03baca62
Habibsultani <a.habibsultani@gmail.com>
2022-01-28 12:05:21 +0430

    git init

--- Script.js
+++ Script.js
@@ -1,3 +1,11 @@
 console.log(12 + 65 + 56 + 645);
 let js = "Amazing";
 console.log(js);
+
+//This is my first time I code in javaScript..
+
+let firstName = "Habib";
+let lastName = "Sultani";
+
+console.log(firstName);
+console.log(lastName);
